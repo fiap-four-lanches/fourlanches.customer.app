836614f9303d510d53598f2347acad989ea912b5
Augusto Silva <augusto.a.silva@hotmail.com>
2024-03-26 23:28:21 -0300

    fix: corrige arquivos de conf do flyway, application e migrations

--- src/main/resources/db/migration/V1__Create_Customer_Table.sql
+++ src/main/resources/db/migration/V1__Create_Customer_Table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE customers (
-    id  SERIAL PRIMARY KEY NOT NULL,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    document varchar(255) NOT NULL,
-    UNIQUE(email, document)
+CREATE TABLE customers
+(
+    id         SERIAL PRIMARY KEY NOT NULL,
+    first_name varchar(255)       NOT NULL,
+    last_name  varchar(255)       NOT NULL,
+    email      varchar(255)       NOT NULL,
+    document   varchar(255)       NOT NULL,
+    UNIQUE (email, document)
 );
